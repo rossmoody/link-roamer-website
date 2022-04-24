15326f7782466d2afa1b6528bb312bba557756f9
rossmoody <fiftyfivehis@gmail.com>
2022-04-24 16:42:48 -0700

    og-image

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,31 @@ import HighlightSection from '../components/HighlightSection'
 import Layout from '../components/Layout'
 import Main from '../components/Main'
 
+const meta = {
+  title: 'Link Roamer',
+  description:
+    'A free and open-source browser extension for finding, organizing, inspecting, bookmarking, grouping, and exporting all the links on a page.',
+  author: '@_rossmoody',
+  url: 'https://www.linkroamer.com',
+}
+
 const Home: NextPage = () => {
   return (
     <React.Fragment>
       <Head>
-        <title>Link Roamer - Quickly handle all the links on a web page</title>
-        <meta
-          name="description"
-          content="A free and open-source browser extension for finding, organizing, inspecting, bookmarking, grouping, and exporting all the links from a page."
-        />
+        <html lang="en" />
+        <meta charSet="utf-8" />
+        <link rel="canonical" href={meta.url} />
+        <title>{meta.title}</title>
+        <meta property="og:title" content={meta.title} />
+        <meta name="description" content={meta.description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={meta.author} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:image" content={`${meta.url}/og-image.png`} />
+        <meta property="og:image" content={`${meta.url}/og-image.png`} />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       </Head>
       <Layout>
         <Main />
